b5f377e77448bf291d7ff902fd9955353302f266
root <root@ci-puppetmaster.elasticdb.org>
2014-10-30 21:02:00 +0000

    moving into cron runnin world.  remove exit from run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -14,8 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-exit
-
 cd /opt/config/production
 git fetch -a && git reset -q --hard @{u}
 ./install_modules.sh
