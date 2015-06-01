67ce2f9b838c2d5ed09044310447580c44597a7c
Brian Hunter <bhunter@tesora.com>
2015-06-01 12:04:50 -0400

    add /etc/hosts entries
    Add static mapping of hostnames to IPs at bluebox.
    (I don't think this much of a security risk)
    
    This facilitates the replacing of a node, where all nodes can be
    mapped to use a new VM.
    
    Change-Id: I8091b1be90c0e006a49ff860a1c8f89ef3e0adba

---
+++ modules/tesora_cyclone/manifests/hosts.pp
@@ -0,0 +1,35 @@
+# == Class: tesora_cyclone::hosts
+#
+class tesora_cyclone::hosts (
+) {
+
+  host { 'review.elasticdb.org':
+    ip => '10.240.28.3',
+    #host_aliases => 'review',
+  }
+
+  host { 'zuul.elasticdb.org':
+    ip => '10.240.28.36',
+  }
+
+  host { 'jenkins01.elasticdb.org':
+    ip => '10.240.28.32',
+  }
+
+  host { 'ci-backup-01.elasticdb.org':
+    ip => '10.240.28.14',
+  }
+
+  host { 'static.elasticdb.org':
+    ip => '10.240.28.44',
+  }
+
+  host { 'git01.elasticdb.org':
+    ip => '10.240.28.4',
+  }
+
+  host { 'ci-puppetmaster.openstacklocal':
+    ip => '10.240.28.27',
+  }
+
+}
