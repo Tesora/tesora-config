288ccebd9c585b7c48a68d550315b1217bc273e0
Brian Hunter <bhunter@tesora.com>
2017-02-23 08:48:24 -0500

    update IPs for symp dev cloud
    Change-Id: Ief8ea5722635b6824863426900a4caeda5148573

--- modules/tesora_cyclone/manifests/hosts.pp
+++ modules/tesora_cyclone/manifests/hosts.pp
@@ -4,31 +4,31 @@ class tesora_cyclone::hosts (
 ) {
 
   host { 'review.elasticdb.org':
-    ip => '10.242.0.215',
+    ip => '10.47.232.52',
   }
 
   host { 'zuul.elasticdb.org':
-    ip => '10.242.0.216',
+    ip => '10.47.232.46',
   }
 
   host { 'jenkins01.elasticdb.org':
-    ip => '10.242.0.238',
+    ip => '10.47.232.48',
   }
 
   host { 'nodepool.elasticdb.org':
-    ip => '10.242.0.240',
+    ip => '10.47.232.53',
   }
 
   host { 'ci-backup-01.elasticdb.org':
-    ip => '10.242.0.253',
+    ip => '10.47.232.55',
   }
 
   host { 'static.elasticdb.org':
-    ip => '10.242.0.246',
+    ip => '10.47.232.54',
   }
 
   host { 'ci-puppetmaster.openstacklocal':
-    ip => '10.242.0.208',
+    ip => '10.47.232.49',
   }
 
 }
